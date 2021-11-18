c1e2bb40fd14f28eb6e864b72ffa60f89bc23241
Andres2D <andres2d1997.1206@gmail.com>
2021-11-18 10:01:05 -0500

    create state to the question and add Error component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,44 @@
-import {Fragment} from 'react';
+import {Fragment, useState} from 'react';
+import Error from './Error';
 
 const Question = () => {
+
+    // Define the state
+    const [value, saveValue] = useState(0);
+    const [error, saveError] = useState(false);
+
+    // Function to get the budget
+    const defineBudget = ({target}) => {
+        saveValue(Number(target.value));
+    }
+
+    // Submit to define the budget
+    const addBudget = e => {
+        e.preventDefault();
+
+        // Validate
+        if(value < 1 || isNaN(value)) {
+            saveError(true);
+            return;
+        }
+
+        saveError(false);
+    }
+
     return ( 
         <Fragment>
             <h2>Input your budget</h2>
 
-            <form>
+            {error ? <Error message="The budget in invalid" /> : null}
+
+            <form
+                onSubmit={addBudget}
+            >
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Your budget"
+                    onChange={defineBudget}
                 />
 
                 <input 
